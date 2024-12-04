976e4f32278a2f062c11e3465e1131b374871c21
Matt Crosby <mattcrosby01@gmail.com>
2024-12-03 22:53:44 -0500

    making create account page

---
+++ frontEnd/loginScript.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    //this part adds event listiners for redirection to different html pages
+    document.getElementById("createAccount").addEventListener('click', function() {
+        window.location.href = './createAccount.html'
+    });
+}
