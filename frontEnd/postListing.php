53ac94d7f682ef77f5af7412211133608be90a33
Matt Crosby <mattcrosby01@gmail.com>
2024-12-04 23:55:52 -0500

    f

--- frontEnd/postListing.php
+++ frontEnd/postListing.php
@@ -63,7 +63,7 @@ else {
   //  $carSellerID = $_GET["sellerID"]; // ***may need to be changed
   // $carSellerID = session()->get("userID");
    $sqlstatement = $conn->prepare("INSERT INTO sleazCarListing values(?, ?, ?, ?, ?, ?, ?, ?)"); //prepare the statement
-   $sqlstatement->bind_param("sssssssss",$listingID,$carMake,$carModel,$carYear,$carColor,$carMileage,$carAskingPrice,$carDescription); //insert the variables into the ? in the above statement // "sssssssss" may not be correct
+   $sqlstatement->bind_param("issisiis",$listingID,$carMake,$carModel,$carYear,$carColor,$carMileage,$carAskingPrice,$carDescription); //insert the variables into the ? in the above statement // "sssssssss" may not be correct
    $sqlstatement->execute(); //execute the query
    echo $sqlstatement->error; //print an error if the query fails
    $sqlstatement->close();
