2f8036bb9a7ee73470cc85cd96d3b63116ca15b6
Matt Crosby <mattcrosby01@gmail.com>
2024-12-05 10:23:33 -0500

    f

--- frontEnd/postListing.php
+++ frontEnd/postListing.php
@@ -30,7 +30,7 @@ include '../backEnd/config.inc'; //change file path for config.inc if needed
 <p><h2>Post Listing Form :</h2></p>
 <form action="postListing.php" method=get>
     <!-- create car ID -->
-	  <>Enter Car Make: <input type="text" size=20 name="make">
+	  <p>Enter Car Make: <input type="text" size=20 name="make">
 	  <p>Enter Car Model: <input type="text" size=5 name="model">
 	  <p>Enter Car Year: <input type="number" size=5 name="year">
     <p>Enter Car Color: <input type="text" size=5 name="color">
@@ -62,7 +62,7 @@ else {
   $carAskingPrice = $_GET["askingPrice"];
   $carDescription = $_GET["description"];
   //  $carSellerID = $_GET["sellerID"]; // ***may need to be changed
-  $carSellerID = $_SESSION["userID"]; //should grad the ID forom the current session
+  $sellerID = $_SESSION["userID"]; //should grad the ID forom the current session
   $sqlstatement = $conn->prepare("INSERT INTO sleazCarListing (make, model, year, color, mileage, askingPrice, description, sellerID) values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
   $sqlstatement->bind_param("ssisiisi",$carMake,$carModel,$carYear,$carColor,$carMileage,$carAskingPrice,$carDescription,$sellerID); //insert the variables into the ? in the above statement // "sssssssss" may not be correct
   if($sqlstatement->execute()) { //execute the query
